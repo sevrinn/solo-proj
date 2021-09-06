fe6f275c8b7daffb36b5d5819b023f5eb3fd88ec
Sevrinn <sevrinnsevrinn@gmail.com>
2021-09-05 19:13:07 -0700

    added bcrypt, cookie-parser, and jsonwebtoken for login/registration

---
+++ server/server.js
@@ -0,0 +1,20 @@
+//start loading with the .env file
+//this way we can use it everywhere in our server
+
+//import express and other libraries
+const express = require("express");
+const app = express();
+
+//declare port
+
+//configure express server
+
+//add cors/ ability to use credentials with cookies
+
+//configure server to accept and update cookies
+
+//configure mongoose to connect
+
+//add routes to listen
+
+//set server listening
