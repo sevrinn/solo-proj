5cd938ee0517cbe82f4404d49192497a1ce574c3
Sevrinn <sevrinnsevrinn@gmail.com>
2021-09-06 08:17:37 -0700

    bands display in a list

--- client/src/components/AllBands.js
+++ client/src/components/AllBands.js
@@ -1,9 +1,30 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Link, navigate } from "@reach/router";
+import BandDetails from "./BandDetails";
 
 const AllBands = (props) => {
-  return <div>All Bands</div>;
+  const [allBands, setAllBands] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8000/api/bands")
+      .then((res) => {
+        console.log(res);
+        setAllBands(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+  return (
+    <div>
+      <h2>All Bands</h2>
+      {allBands.map((band, index) => (
+        <div key={index}>{band.bandName}</div>
+      ))}
+    </div>
+  );
 };
 
 export default AllBands;
